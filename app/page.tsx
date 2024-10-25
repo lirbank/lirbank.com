23eddf9b2067686bcc7f7844f9113d89e72e8fa1
Mikael Lirbank <mikael@lirbank.com>
2024-10-25 11:19:59 -0700

    UI WIP

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,58 @@
 import Image from "next/image";
 import mikaelImage from "../public/mikael-lirbank.jpg";
+import { LinkedInIcon, SquareGitHubIcon, SquareXTwitterIcon } from "./icons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center gap-10 p-10 text-slate-800">
-      <h1 className="max-w-screen-md text-center text-4xl font-semibold sm:text-6xl">
-        Unleash your app&apos;s true potential
-      </h1>
-      <Image
-        className="size-64 rounded-full border-slate-200 object-cover"
-        src={mikaelImage}
-        alt="Mikael Lirbank"
-      />
-      <div>
-        <h2 className="text-lg font-semibold">Mikael Lirbank</h2>
-        <p>Tech lead, product manager, code quality expert</p>
-      </div>
+    <main className="min-h-dvh bg-stone-300 text-slate-800">
+      <section className="flex flex-col gap-10 p-10">
+        <h1 className="sm:text-6xl_ mx-auto text-center text-4xl font-semibold">
+          Unleash your app&apos;s true potential
+        </h1>
+        <Image
+          className="mx-auto size-64 rounded-full object-cover"
+          src={mikaelImage}
+          alt="Mikael Lirbank"
+        />
+        <div className="flex flex-col gap-2">
+          <div>
+            <h3 className="text-lg font-semibold">Mikael Lirbank</h3>
+            {/* <p>Tech lead, product manager, code quality expert</p> */}
+            <p>I help companies build better software</p>
+          </div>
+          <div className="flex gap-2">
+            <LinkedInIcon className="size-8" />
+            <SquareXTwitterIcon className="size-8" />
+            <SquareGitHubIcon className="size-8" />
+          </div>
+        </div>
+      </section>
+      {/* <section className="flex flex-col gap-10 bg-stone-300 p-10">
+        <div>
+          <h3 className="text-lg font-semibold">What I do</h3>
+          <p>I help companies build better software.</p>
+        </div>
+      </section> */}
+      <section className="flex flex-col gap-10 rounded bg-stone-200 p-10">
+        <h2 className="text-2xl font-semibold">
+          Things I&apos;d love to build for you
+        </h2>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-semibold">Build from scratch</h3>
+          <p>Ipsums dolor sit amet.</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-semibold">JavaScript to TypeScript</h3>
+          <p>
+            TypeScript will boost productivity and reduce bugs. If your app is
+            not written in TypeScript, I can convert it for you.
+          </p>
+        </div>
+      </section>
     </main>
   );
+}
+
+{
+  /* <div className="text-base">Some stuff</div> */
 }
