c35479658ad00a2853d2435837021a4b44c23a93
Mikael Lirbank <mikael@lirbank.com>
2024-11-01 10:30:25 -0700

    TS

--- chrome/background.ts
+++ chrome/background.ts
@@ -1,12 +1,16 @@
+/* eslint-disable @typescript-eslint/no-deprecated */
+import invariant from "tiny-invariant";
 console.log("Background service worker initialized!");
 
 // Log all registered commands on startup with JSON.stringify
-chrome.commands.getAll().then((commands) => {
+void chrome.commands.getAll().then((commands) => {
   console.log("Registered commands:", JSON.stringify(commands, null, 2));
 });
 
 // This handler is called for both keyboard shortcut and icon click
 chrome.action.onClicked.addListener((tab) => {
+  invariant(tab.id, "tab.id is required");
+
   console.log("Action triggered!", {
     source: "Keyboard shortcut or icon click",
     tabId: tab.id,
@@ -14,21 +18,27 @@ chrome.action.onClicked.addListener((tab) => {
   });
 
   // Inject and execute the content script
-  chrome.scripting.executeScript({
+  void chrome.scripting.executeScript({
     target: { tabId: tab.id },
     func: () => {
       const selector = "#profile-content h1";
-      const element = document.querySelector(selector);
+      const element: HTMLElement | null = document.querySelector(selector);
 
       if (element) {
         const text = element.innerText || element.textContent;
+        invariant(text, "text is required");
+
         const url = window.location.href;
         const formattedText = `${text}\t${url}`;
 
         navigator.clipboard
           .writeText(formattedText)
-          .then(() => console.log("Text and URL copied to clipboard!"))
-          .catch((err) => console.error("Failed to copy text and URL:", err));
+          .then(() => {
+            console.log("Text and URL copied to clipboard!");
+          })
+          .catch((err) => {
+            console.error("Failed to copy text and URL:", err);
+          });
       } else {
         console.error("Element not found:", selector);
       }
