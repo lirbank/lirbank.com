c024bea56f97fe39843cc62748a1870c45ea2c94
Mikael Lirbank <mikael@lirbank.com>
2024-11-03 13:32:23 -0800

    Update atoms.tsx

--- app/atoms.tsx
+++ app/atoms.tsx
@@ -25,7 +25,7 @@ export function Testimonial(props: {
   };
 }) {
   return (
-    <>
+    <div className="-mx-4 mt-8 rounded-md bg-stone-50 p-4">
       <h3>{props.project.name}</h3>
       <div className="text-lg text-stone-600">
         {props.project.url ? (
@@ -57,12 +57,11 @@ export function Testimonial(props: {
             </div>
           )}
         </A>
-
         <div className="flex flex-col">
           <strong className="text-lg">{props.author.name}</strong>
           <div className="text-stone-700">{props.author.title}</div>
         </div>
       </div>
-    </>
+    </div>
   );
 }
