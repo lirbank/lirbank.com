5e86d52b19d9d23cbe4ef8fe02fb892819818935
Mikael Lirbank <mikael@lirbank.com>
2024-11-05 11:43:55 -0800

    Update background.ts

--- chrome/background.ts
+++ chrome/background.ts
@@ -24,33 +24,30 @@ chrome.action.onClicked.addListener((tab) => {
     target: { tabId: tab.id },
     func: () => {
       const selector = "h1";
-      const element: HTMLElement | null = document.querySelector(selector);
+      const element = document.querySelector(selector);
 
-      if (element) {
-        const text = element.innerText || element.textContent;
-
-        if (!text) {
-          console.error("text is required");
-          return;
-        }
-
-        // Get the origin and pathname of the current URL (without query params)
-        const url =
-          new URL(window.location.href).origin +
-          new URL(window.location.href).pathname;
-        const formattedText = `${text}\t${url}`;
-
-        navigator.clipboard
-          .writeText(formattedText)
-          .then(() => {
-            console.log("Text and URL copied to clipboard!");
-          })
-          .catch((err) => {
-            console.error("Failed to copy text and URL:", err);
-          });
-      } else {
+      if (!element) {
         console.error("Element not found:", selector);
+        return;
       }
+
+      const text = element.innerText;
+
+      // Get the origin and pathname of the current URL (without query params)
+      const url =
+        new URL(window.location.href).origin +
+        new URL(window.location.href).pathname;
+
+      const formattedText = `${text}\t${url}`;
+
+      navigator.clipboard
+        .writeText(formattedText)
+        .then(() => {
+          console.log("OK");
+        })
+        .catch((err) => {
+          console.error("Failure:", err);
+        });
     },
   });
 });
