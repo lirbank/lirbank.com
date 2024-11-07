06ec0388156a5df8f2b540cb47b3d735e9a489b2
Mikael Lirbank <mikael@lirbank.com>
2024-11-06 17:34:31 -0800

    WIP

--- chrome/background.ts
+++ chrome/background.ts
@@ -33,14 +33,18 @@ chrome.action.onClicked.addListener((tab) => {
       }
 
       const text = element.innerText;
-
-      // Get the origin and pathname of the current URL (without query params)
       const url =
         new URL(window.location.href).origin +
         new URL(window.location.href).pathname;
-
       const formattedText = `${text}\t${url}`;
 
+      // Send data to background script instead of handling IndexedDB here
+      void chrome.runtime.sendMessage({
+        type: "SAVE_CLIP",
+        data: { text, url, formattedText },
+      });
+
+      // Continue with clipboard operation
       navigator.clipboard
         .writeText(formattedText)
         .then(() => {
@@ -52,3 +56,62 @@ chrome.action.onClicked.addListener((tab) => {
     },
   });
 });
+
+// Add message listener for IndexedDB operations
+chrome.runtime.onMessage.addListener((message) => {
+  console.log("message", message);
+
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  if (message.type === "SAVE_CLIP") {
+    const request = indexedDB.open("ClipboardHistory", 1);
+
+    request.onerror = (event) => {
+      console.error("IndexedDB error:", event);
+    };
+
+    request.onupgradeneeded = (event) => {
+      const db = (event.target as IDBOpenDBRequest).result;
+      if (!db.objectStoreNames.contains("clips")) {
+        db.createObjectStore("clips", { keyPath: "timestamp" });
+      }
+    };
+
+    request.onsuccess = (event) => {
+      const db = (event.target as IDBOpenDBRequest).result;
+      const transaction = db.transaction(["clips"], "readwrite");
+      const store = transaction.objectStore("clips");
+
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+      const clip = {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+        ...message.data,
+        timestamp: new Date().getTime(),
+      };
+
+      console.log("clip", clip);
+
+      store.add(clip);
+
+      getAllClips();
+    };
+  }
+});
+
+// Add this function to help debug
+function getAllClips() {
+  const request = indexedDB.open("ClipboardHistory", 1);
+
+  request.onsuccess = (event) => {
+    const db = (event.target as IDBOpenDBRequest).result;
+    const transaction = db.transaction(["clips"], "readonly");
+    const store = transaction.objectStore("clips");
+    const getAllRequest = store.getAll();
+
+    getAllRequest.onsuccess = () => {
+      console.log("All clips:", getAllRequest.result);
+    };
+  };
+}
+
+// Call it after each save or manually in the console
+// getAllClips();
