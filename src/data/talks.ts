d387b0f23131aa27560aaf1e41ab1f191b556bfa
Mikael Lirbank <mikael@lirbank.com>
2026-07-07 09:12:28 -0700

    Talks

--- src/data/talks.ts
+++ src/data/talks.ts
@@ -2,7 +2,6 @@
 interface Talk {
   date: string;
   title: string;
-  href: string;
   note?: string;
   venue: string;
   links: { label: string; href: string }[];
@@ -12,21 +11,25 @@ export const talks: Talk[] = [
   {
     date: "Apr 3, 2025",
     title: "AI x All Things Web",
-    href: "https://allthingsweb.dev/2025-04-03-ai-x-all-things-web",
-    note: "confirmed",
     venue: "AWS Gen AI Loft, San Francisco, CA",
     links: [
-      { label: "Register", href: "https://lu.ma/9dga4f43?tk=QkmdTN" },
       {
         label: "Presentation deck",
         href: "https://www.figma.com/deck/eJwtTQUnW3Zvy5994vM5qR",
+      },
+      {
+        label: "Event page",
+        href: "https://lu.ma/9dga4f43",
+      },
+      {
+        label: "All Things Web",
+        href: "https://allthingsweb.dev/2025-04-03-ai-x-all-things-web",
       },
     ],
   },
   {
     date: "Dec 2, 2024",
     title: "SpikeGadgets",
-    href: "https://spikegadgets.com/",
     note: "private event",
     venue: "SpikeGadgets HQ, San Francisco, CA",
     links: [
@@ -34,21 +37,10 @@ export const talks: Talk[] = [
         label: "Presentation deck",
         href: "https://www.figma.com/deck/qUZ9ZiFV0RQfYUMdWjeFBf",
       },
+      {
+        label: "Website",
+        href: "https://spikegadgets.com/",
+      },
     ],
-  },
-  {
-    date: "Feb 25, 2025",
-    title: "All Things Web",
-    href: "https://allthingsweb.dev/2025-02-25-all-things-web-at-sentry",
-    note: "rescheduled",
-    venue: "Sentry HQ, San Francisco, CA",
-    links: [],
-  },
-  {
-    date: "Mar 2025, TBA",
-    title: "Silicon Valley AI Think Tank",
-    href: "https://lu.ma/JTA",
-    venue: "Menlo Park, CA",
-    links: [],
   },
 ];
